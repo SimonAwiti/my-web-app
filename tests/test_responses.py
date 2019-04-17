737767c351a8fedd28173a54e3a1244febb70ebc
SimonAwiti <awitisimon23@gmail.com>
2019-04-17 14:35:03 +0300

    all tests

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -24,6 +24,17 @@ class TestResponse(unittest.TestCase):
             "Phone" : "0722334455",
             "message" : "send me your resume/cv",
             }
+        self.response3 = {
+            "name" : "",
+            "email" : "",
+            "Phone" : "",
+            "message" : "",
+            }
+        self.response4 = {
+            "name" : "1",
+            "email" : "1",
+            "message" : "1",
+            }
 
     def test_post_response(self):
         """Tests for posting a new response record"""
@@ -32,3 +43,29 @@ class TestResponse(unittest.TestCase):
                                     content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn("Message succesfully send", str(response.data))
+
+    def test_get_all_responses(self):
+        """Test for getting all responses"""
+        response = self.client.get('/api/v1/responses',
+                                    data=json.dumps(self.response1),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("All responses posted", str(response.data))
+
+    def test_post_response_with_no_field(self):
+        """Test for trying to post a response with no field"""
+        response = self.client.post('/api/v1/responses',
+                                    data=json.dumps(self.response3),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Kindly fill up all the fields", str(response.data))
+
+    def test_post_response_with_digit_fields(self):
+        """Test for trying to post a response with no field"""
+        response = self.client.post('/api/v1/responses',
+                                    data=json.dumps(self.response4
+                                    ),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("The fields should be described in words", str(response.data))
+        
