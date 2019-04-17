9da8c677e71cc1529b402390ef24d2e91616df2f
SimonAwiti <awitisimon23@gmail.com>
2019-04-17 13:51:18 +0300

    updated configurations

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
-"""app initializer """
-
 import os
-
-"""you import this to get all that we had defined and exported in the .env"""
 from app import create_app
-"""this is imported from the __init__.py file contained in the subdirectory called app"""
-config_name = os.getenv("APP_SETTINGS")
-"""Gets the app settings defined in the .env file"""
+
+config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
-"""defining the configuration to be used"""
+
 if __name__ == '__main__':
-app.run()
+    app.run()
